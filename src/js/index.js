1ab66872c92cc75b9012b399c0d1981d1fc9c64c
DorianTrazos <117446752+DorianTrazos@users.noreply.github.com>
2025-04-22 10:53:05 +0200

    updated

--- src/js/index.js
+++ src/js/index.js
@@ -8,7 +8,7 @@ const dayElement = document.getElementById('day');
 const monthElement = document.getElementById('month');
 const yearElement = document.getElementById('year');
 
-const days = ['Sunday', 'Monday', 'Tusday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
